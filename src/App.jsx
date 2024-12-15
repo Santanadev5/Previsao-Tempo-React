03fa21500ec81fdc07f94fb5d2a6ee4990b43f08
Nicolas Santana <nicolassantanadev@gmail.com>
2024-12-15 16:35:37 -0300

    JavaScript assíncrono/aguardado usado para recuperar dados meteorológicos da API Open Weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
-
+import { useState, useRef } from 'react'
+import axios from 'axios'
 
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const inputRef = useRef()
+
+  async function searchCity(){
+    const city = inputRef.current.value
+    const key = "9f16ec955dd66fdff4d3e03bf12ead4b"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
+
+    const data =  await axios.get(url)
+  }
 
   return (
     <div>
 
       <h1>Previsão do Tempo</h1>
+      <input ref={inputRef} type='text' placeholder='Digite o nome da cidade'/>
+      <button onClick={searchCity}>Buscar</button>
       
     </div>
   )
