9c8ecb69108c9c20d5792985d186bb4b61989ec8
Nicolas Santana <nicolassantanadev@gmail.com>
2024-12-15 18:16:33 -0300

    Criando um aplicativo de previsão do tempo do zero usando React, incluindo filtragem e exibição de dados

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useState, useRef } from 'react'
 import axios from 'axios'
 
 import './App.css'
+import WeatherInformations from './components/weatherinformations/weatherinformations'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weather, setWeather] = useState({})
   const inputRef = useRef()
 
   async function searchCity(){
@@ -12,7 +13,9 @@ function App() {
     const key = "9f16ec955dd66fdff4d3e03bf12ead4b"
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
 
-    const data =  await axios.get(url)
+    const apiInfo =  await axios.get(url)
+
+    setWeather (apiInfo.data)
   }
 
   return (
@@ -22,6 +25,8 @@ function App() {
       <input ref={inputRef} type='text' placeholder='Digite o nome da cidade'/>
       <button onClick={searchCity}>Buscar</button>
       
+      <WeatherInformations weather={weather}/>
+
     </div>
   )
 }
