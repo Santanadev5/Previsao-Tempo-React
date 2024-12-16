a7df05e57d22635487a8528159cc39b2a29b15d5
Nicolas Santana <nicolassantanadev@gmail.com>
2024-12-16 01:27:38 -0300

    Ajuste do CSS e dos elementos de estilo no aplicativo React

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import WeatherInformations from './components/weatherinformations/weatherinformations'
 
 function App() {
-  const [weather, setWeather] = useState({})
+  const [weather, setWeather] = useState()
   const inputRef = useRef()
 
   async function searchCity(){
@@ -19,13 +19,13 @@ function App() {
   }
 
   return (
-    <div>
+    <div className='container'>
 
       <h1>Previsão do Tempo</h1>
       <input ref={inputRef} type='text' placeholder='Digite o nome da cidade'/>
       <button onClick={searchCity}>Buscar</button>
       
-      <WeatherInformations weather={weather}/>
+      {weather &&<WeatherInformations weather={weather}/>}
 
     </div>
   )
