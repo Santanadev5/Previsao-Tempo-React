f9e245b8ebf825a9ccd5b1a13c66a17b657f17ae
Nicolas Santana <nicolassantanadev@gmail.com>
2024-12-16 02:09:55 -0300

    Ajustando design e layout, buscando e processando dados meteorológicos

---
+++ src/WeatherInformations5Days/WeatherInformations5Days.jsx
@@ -0,0 +1,17 @@
+import './WeatherInformations5Days.css'
+
+function WeatherInformations5Days ({weather5Days}) {
+
+return (
+
+  <div className='weather-container'>
+
+  <p>5Days</p>
+    
+  </div>
+
+)
+
+}
+
+export default WeatherInformations5Days ;
