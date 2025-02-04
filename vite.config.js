be893e54921cb0d1cce4cf083396af9507470cac
Nicolas Santana <nicolassantanadev@gmail.com>
2025-02-04 16:57:50 -0300

    ajuste nas configurações do projeto para subir no github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Previsao-Tempo-React/',  // Ajuste o caminho para o nome do seu repositório
 })
