9cc4a4b40a79c89865e1be63da7be4ad0968bd65
Nicolas Santana <nicolassantanadev@gmail.com>
2025-02-06 15:39:21 -0300

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Previsao-Tempo-React/',  // Ajuste o caminho para o nome do seu repositório
-})
